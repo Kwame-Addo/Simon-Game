14dd81334771b20f0ea95829c7faf76d8abe0e40
Kwame Okai Addo <48158461+Kwame-Addo@users.noreply.github.com>
2022-12-05 16:04:47 +0000

    Add files via upload

---
+++ game.js
@@ -0,0 +1,47 @@
+
+var buttonColors = ["red","blue","green","yellow"]
+
+
+function nextSequence() {
+    var randomNumber = Math.random() * 4;
+    randomNumber = Math.floor(randomNumber);
+}
+
+var randomChosenColor
+
+
+
+
+
+
+
+
+//
+// var greenAudio = new Audio("sounds/green.mp3");
+// var redAudio = new Audio("sounds/red.mp3");
+// var yellowAudio = new Audio("sounds/yellow.mp3");
+// var blueAudio = new Audio("sounds/blue.mp3");
+//
+// $(document).keypress(function(){
+//   $("h1").html("Level 1");
+//   var randomTile = Math.random() * 4;
+//   randomTile = Math.floor(randomTile + 1);
+//   if (randomTile===1){
+//     greenAudio.play();
+//     $("#green").animate({opacity: 0.1}).animate({opacity:1});
+//   }else if (randomTile===2){
+//     redAudio.play();
+//   $("#red").animate({opacity: 0.1}).animate({opacity:1});
+//   }else if (randomTile===3){
+//     yellowAudio.play();
+//     $("#yellow").animate({opacity: 0.1}).animate({opacity:1});
+//   }else {
+//     blueAudio.play();
+//   $("#blue").animate({opacity: 0.1}).animate({opacity:1});
+//   }
+// })
+//
+//
+// $(".btn").click(function(){
+//   blueAudio.play();
+// })
